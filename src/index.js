78cb2030d06ec16fbd2f01e5269a4ababe047f1f
higorhms <higor_hms@outlook.com>
2023-10-04 11:58:38 -0500

    feat: add eslint

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
-const fastify = require('fastify')({ logger: true })
+const fastify = require('fastify')({ logger: true });
 
-fastify.get('/', function handler (request, reply) {
-  reply.send({ hello: 'world' })
-})
+fastify.get('/', (request, reply) => {
+  reply.send({ hello: 'world' });
+});
 
 fastify.listen({ port: 3000 }, (err) => {
   if (err) {
-    fastify.log.error(err)
-    process.exit(1)
+    fastify.log.error(err);
+    process.exit(1);
   }
-})
+});
